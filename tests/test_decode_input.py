8dc74de405a82ffe73cfc4904fc871e2886008ff
GiuliaGualtieri <giulia.gualtieri@mail.polimi.it>
2023-02-07 22:33:00 +0100

    Update run-test job in ci-cd pipeline

--- tests/test_decode_input.py
+++ tests/test_decode_input.py
@@ -6,5 +6,5 @@ def test_decode_input():
     TXT2 = "J'ai montré mon chef-d'oeuvre aux grandes <mask> et je leur ai demandé si mon dessin leur faisait peur."
     TXT3 = "je faisais l'expérience sur <mask> de mon dessin numéro Un"
     for txt in [TXT, TXT2, TXT3]:
-        print(f_decode_input(txt))
+        f_decode_input(txt)
     return
