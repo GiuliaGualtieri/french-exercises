7ee9213b8593e521c06ee5f3711d37a738ffd80e
GiuliaGualtieri <giulia.gualtieri@mail.polimi.it>
2023-03-05 12:38:32 +0100

    Update df_QA.csv

--- french_exercises/decode_data.py
+++ french_exercises/decode_data.py
@@ -15,50 +15,55 @@ dict_QA = {"question": [], "answer": []}
 
 df = pd.read_csv(upstream["read-data"]["data"])
 
-dict_QA = {'question' : [], 'answer' : [], 'answers' : []}
+dict_QA = {"question": [], "answer": [], "answers": []}
 
 list_punctuation_to_remove = '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~)Ã©ª'
+
 
 def check_prediction(to_mask, predictions):
     list_ok = []
     list_ko = []
     predictions2 = None
     for prediction in predictions:
-        prediction.translate(str.maketrans('', '', list_punctuation_to_remove))
-        if len(prediction)<2:
+        prediction.translate(str.maketrans("", "", list_punctuation_to_remove))
+        if len(prediction) < 2:
             list_ko.append(prediction)
-        elif len(prediction)< 7 and len(prediction)<(len(to_mask)-2):
+        elif len(prediction) < 7 and len(prediction) < (len(to_mask) - 2):
             list_ko.append(prediction)
         else:
             list_ok.append(prediction)
     if list_ko:
-        txt2 = "Un possible synonyme de "+to_mask+" est <mask>."
+        txt2 = "Un possible synonyme de " + to_mask + " est <mask>."
         predictions2 = f_decode_input(txt2)
-        predictions2 = [el.translate(str.maketrans('', '', list_punctuation_to_remove)) for el in predictions2]
-        predictions2 = [el for el in predictions2 if len(el)>1]
+        predictions2 = [
+            el.translate(str.maketrans("", "", list_punctuation_to_remove))
+            for el in predictions2
+        ]
+        predictions2 = [el for el in predictions2 if len(el) > 1]
         list_ok.extend(predictions2)
     return list_ok
 
+
 for row in df.iterrows():
-    words = row[1]['new'].split()
-    rnd = random.randint(0,len(words)-1)
+    words = row[1]["new"].split()
+    rnd = random.randint(0, len(words) - 1)
     to_mask = words[rnd]
-    while len(to_mask)<2:
-        rnd = random.randint(0,len(words)-1)
-        to_mask = words[rnd]    
-    words[rnd] = '<mask>'
-    to_txt = ''
+    while len(to_mask) < 2:
+        rnd = random.randint(0, len(words) - 1)
+        to_mask = words[rnd]
+    words[rnd] = "<mask>"
+    to_txt = ""
     for el in words:
-        to_txt += el + ' '
+        to_txt += el + " "
     txt = to_txt[:-1]
     predictions = f_decode_input(txt)
     predictions = check_prediction(to_mask, predictions)
     if to_mask not in predictions:
         predictions = predictions[:-1]
         predictions.append(to_mask)
-    dict_QA['question'].append(txt)
-    dict_QA['answer'].append(to_mask)
-    dict_QA['answers'].append(predictions)
+    dict_QA["question"].append(txt)
+    dict_QA["answer"].append(to_mask)
+    dict_QA["answers"].append(predictions)
 
 # %%
 df["question"] = dict_QA["question"]
